ee04ffd7266bcd0eb41301b02597b5b88ab317eb
andrewtronn <andreeewtron@yahoo.com>
2018-12-14 14:51:05 -0800

    added header

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Header = (props) => {
+    return(
+        <div className="head-container">
+            <h1>FOOD APP</h1>
+        </div>
+    )
+}
+export default Header; 
