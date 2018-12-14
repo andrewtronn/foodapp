06facb96b070c64cae1fb19554621dd30ae22b26
andrewtronn <andreeewtron@yahoo.com>
2018-12-14 15:53:36 -0800

    added header

--- src/App.js
+++ src/App.js
@@ -2,16 +2,25 @@ import React, { Component } from 'react';
 import Header from './components/Header';
 import './App.css';
 
+
+
+
+
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+        <Header />
 
         </header>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,15 +2,18 @@ import { LOADED_FILE } from '../constants'
 
 import axios from 'axios';
 
-var config = {
-    headers: {'X-My-Custom-Header': 'e72ea3a9fe87325d55dd0b6d7b2a010f'}
-   };
+// var config = {
+//     headers: {'X-My-Custom-Header': 'e72ea3a9fe87325d55dd0b6d7b2a010f'}
+//    };
 
-export const getfile = (id) => dispatch => 
-axios.get("https://developers.zomato.com/api/v2.1/categories",config) 
-    .then(res => {
-        dispatch(sendData(LOADED_FILE,res.data))
-    })
+// export const getfile = () => dispatch => 
+// axios.get("https://developers.zomato.com/api/v2.1/categories",config) 
+//     .then(res => {
+//         // dispatch(sendData(LOADED_FILE,res.data))
+//         let data= res
+        
+        
+//     })
 
 
 export const sendData = (type, data) => ({type:type, payload: data})
